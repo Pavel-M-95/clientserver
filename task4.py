7cc703ad0ecdba1125dceb19d4d429e147249b11
Pavel-M-95 <pavlyxa95@gmail.com>
2022-01-10 20:44:50 +0300

    lesson2

--- task4.py
+++
@@ -1,8 +0,0 @@
-str = ['разработка', 'администрирование', 'protocol', 'standard']
-
-for i in str:
-    a = i.encode('utf-8')
-    print(a, type(a))
-    b = bytes.decode(a, 'utf-8')
-    print(b, type(b))
-    print('#' * 30)
